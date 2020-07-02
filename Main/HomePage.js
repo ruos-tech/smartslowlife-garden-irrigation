772440dafcde57b001e3a69180e29d4466d0bcf0
fick <fick707@qq.com>
2020-07-02 23:50:17 +0800

    swithc control impl

--- Main/HomePage.js
+++ Main/HomePage.js
@@ -1,9 +1,9 @@
 'use strict';
 import React, { Component } from 'react';
-import { Package, Host } from 'miot';
 import NavigationBar from "miot/ui/NavigationBar";
-import Separator from 'miot/ui/Separator';
-import { ScrollView, View, Text } from 'react-native';
+import Switch from 'miot/ui/Switch';
+import { Service, Device } from "miot";
+import { View, StyleSheet,Text } from 'react-native';
 
 export default class HomePage extends React.Component {
 
@@ -19,9 +19,11 @@ export default class HomePage extends React.Component {
     super(props);
     this.state = {
       transparent: true,
+      device: Device,
       backgroundColor: '#fff',
       showDot: true,
       index: 0,
+      pro2_1: false,
     }
   }
 
@@ -30,15 +32,22 @@ export default class HomePage extends React.Component {
     return (
       <NavigationBar
         backgroundColor='transparent'
+        subtitle='智慧灌溉系统'
         // type={NavigationBar.TYPE.DARK}
         left={[
           {
             key: NavigationBar.ICON.BACK,
-            onPress: _ => this.props.navigation.goBack()
+            onPress: _ => {
+              console.log("go back.");
+              this.props.navigation.goBack();
+            }
           },
           {
             key: NavigationBar.ICON.CLOSE,
-            onPress: _ => this.props.navigation.goBack()
+            onPress: _ => {
+              console.log("go close.");
+              this.props.navigation.goBack()
+            }
           }
         ]}
         right={[
@@ -52,27 +61,97 @@ export default class HomePage extends React.Component {
             onPress: _ => console.log('onPress more')
           }
         ]}
-        title='智慧慢生活-花园-灌溉系统'
-        subtitle=''
+        title='智慧慢生活-智慧花园'
         onPressTitle={_ => console.log('onPressTitle')}
       />
+
     );
+  }
+
+  componentDidMount() {
+    console.log("device info:");
+    console.log(this.state.device);
+    let params = [{
+      did: this.state.device.deviceID,
+      siid: 2,
+      piid: 1
+    }];
+    Service.spec.getPropertiesValue(params).then(res => {
+      console.log("res", res);
+      // res.
+      // this.setState({})
+    }).catch(error => {
+        console.log("error", error)
+    });
+  }
+
+  changeSwitch(value){
+    let params = [{
+      did: this.state.device.deviceID,
+      siid: 2,
+      piid: 1
+    }];
+    Service.spec.setPropertiesValue(params).then(res => {
+      console.log("res", res)
+    }).catch(error => {
+        console.log("error", error)
+    });
+    this.setState({pro2_1:value});
   }
 
   render() {
     return (
       <View style={{ backgroundColor: this.state.backgroundColor, flex: 1 }}>
         {this.renderCustomNavigation()}
-        <Separator />
-        <ScrollView>
-          <Text>Hello, Ruos Tech Smart Slow Life.</Text>
-          <Text>API_LEVEL:{Package.minApiLevel}</Text>
-          <Text>NATIVE_API_LEVEL:{Host.apiLevel}</Text>
-          <Text>{Package.packageName}</Text>
-          <Text>models:{Package.models}</Text>
-        </ScrollView>
+        <View style={styles.mainContent}>
+          <Text>更多内容，敬请期待,{this.state.device.deviceID}</Text>
+        </View>
+        <View style={styles.footer}>
+          <View style={styles.switchArea}>
+            <Text style={styles.switchText}>水龙头开关</Text><Switch style={styles.switchIcon} 
+              onTintColor='skyblue'
+              tintColor='lightpink'
+              value={this.state.pro2_1}
+              onValueChange={value => this.changeSwitch(value)}
+              />
+          </View>
+        </View>
       </View>
     )
   }
+}
 
-};
+var styles = StyleSheet.create({
+  mainContent: {
+    marginTop: 25,
+    marginLeft: 25,
+  },
+  footer: {
+    width: '100%',
+    height: 100,
+    bottom: 0,
+    position: "absolute",
+    fontSize: 14,
+    borderColor: '#EFEFEF',
+    borderWidth: 1,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20
+  },
+  switchArea: {
+  marginTop: 35,
+  marginLeft: 25,
+  marginRight: 25,
+  flexDirection: 'row',
+  flexWrap: 'nowrap',
+  justifyContent: 'space-between'
+  },
+  switchText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  switchIcon: {
+    width: 50, 
+    height: 25,
+    marginRight: 25
+  }
+});
