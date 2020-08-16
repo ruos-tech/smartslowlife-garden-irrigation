aeafba908c3e66a51aa5c92769237a3c5e78198f
fick <fick707@qq.com>
2020-08-16 12:16:37 +0800

    1. led display ; 2. auto stop minutes; 3. add backgroud pic;

--- Main/HomePage.js
+++ Main/HomePage.js
@@ -5,8 +5,24 @@ import {
   MessageDialog
 } from 'miot/ui/Dialog';
 import Switch from 'miot/ui/Switch';
+import NumberSpinner from 'miot/ui/NumberSpinner';
 import { Service, Device, Package,DeviceEvent } from "miot";
-import { View, StyleSheet,Text } from 'react-native';
+import { View, StyleSheet,Text,Dimensions, ScrollView, TouchableHighlight, Image } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+
+/**
+ * iOS: windowHeight === screenHeight
+ * android: screenHeight - windowHeight = 底部导航条（非全面屏下虚拟按键）的高度
+ */
+const { width: windowWidth, height: windowHeight } = Dimensions.get('window'); // 视窗尺寸
+const { width: screenWidth, height: screenHeight } = Dimensions.get('screen'); // 屏幕尺寸
+const statusBarHeight = getStatusBarHeight(true); // 状态栏高度
+const titleBarHeight = 44; // 顶部导航栏的高度，在 `TitleBar` 中有定义 TODO: TitleBar 提供静态方法获取导航栏高度
+const backgroundContentHeight = windowHeight - 44 - 90; // 背景层的高度
+const safeAreaHeight = 0; // 安全区域的高度
+const contentInsetBottom = backgroundContentHeight + safeAreaHeight; // ScrollView 内容固定的位置
+const scrollViewHeight = windowHeight - statusBarHeight - titleBarHeight; // ScrollView 的高度
 
 export default class HomePage extends React.Component {
 
@@ -22,19 +38,25 @@ export default class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      scrollMode: false,
       transparent: true,
+      contentInsetY: contentInsetBottom,
       device: Device,
       backgroundColor: '#fff',
       showDot: true,
       index: 0,
       // pro2_1: 水龙头开关
       pro2_1: false,
+      // pro6_1: led_display
+      pro6_1: false,
+      pro6_2: 5,
       // pro3_3: 温度
       pro3_3: 25.0,
       // pro3_4:相对湿度
       pro3_4: 23.1,
       messageDialog: false,
     }
+    this.myRef = React.createRef()
   }
 
   UNSAFE_componentWillMount() {
@@ -99,7 +121,18 @@ export default class HomePage extends React.Component {
       did: this.state.device.deviceID,
       siid: 2,
       piid: 1
-    }];
+    },
+    {
+      did: this.state.device.deviceID,
+      siid: 6,
+      piid: 1
+    },
+    {
+      did: this.state.device.deviceID,
+      siid: 6,
+      piid: 2
+    }
+    ];
     Service.spec.getPropertiesValue(params).then(res => {
       // console.log("res", res);
       res.forEach(element => {
@@ -116,8 +149,6 @@ export default class HomePage extends React.Component {
     }).catch((err) => {
       // console.log(err);
     });
-
-
 
 
   }
@@ -140,6 +171,42 @@ export default class HomePage extends React.Component {
     });
   }
 
+  changeLedDisplay(value){
+    console.log('change led display to '+value);
+    let params = [{
+      did: this.state.device.deviceID,
+      siid: 6,
+      piid: 1,
+      value
+    }];
+    Service.spec.setPropertiesValue(params).then(res => {
+      res.forEach(element => {
+        element.value = value;
+        this.syncProperty(element);
+      });
+    }).catch(error => {
+        console.log("set property error", error)
+    });
+  }
+
+  changeAutoStop(value){
+    console.log('change auto stop to '+value);
+    let params = [{
+      did: this.state.device.deviceID,
+      siid: 6,
+      piid: 2,
+      value
+    }];
+    Service.spec.setPropertiesValue(params).then(res => {
+      res.forEach(element => {
+        element.value = value;
+        this.syncProperty(element);
+      });
+    }).catch(error => {
+        console.log("set property error", error)
+    });
+  }
+
   syncProperty(element){
     if(element.code === 0){
       let v = element.value;
@@ -147,6 +214,16 @@ export default class HomePage extends React.Component {
         if(element.piid === 1){
           this.setState({
             pro2_1:v
+          });
+        }
+      } else if(element.siid === 6){
+        if(element.piid === 1){
+          this.setState({
+            pro6_1:v
+          });
+        } else if(element.piid === 2){
+          this.setState({
+            pro6_2:v
           });
         }
       }
@@ -172,6 +249,8 @@ export default class HomePage extends React.Component {
           </View>
         </View>
          */}
+
+        <Image style={styles.flower} source={require('./Resources/images/flower.png')} />
         <MessageDialog
           type={MessageDialog.TYPE.UNDERLINE}
           message={this.state.pro2_1 ? '开始浇水了':'停止浇水了'}
@@ -190,16 +269,18 @@ export default class HomePage extends React.Component {
           }}
           visible={this.state.messageDialog}
         />
-        <View style={styles.footer}>
-          <View style={styles.switchArea}>
-            <Text style={styles.switchText}>水龙头开关</Text><Switch style={styles.switchIcon} 
-              onTintColor='skyblue'
-              tintColor='lightgrey'
-              value={this.state.pro2_1}
-              onValueChange={value => this.changeSwitch(value)}
-              />
-          </View>
-        </View>
+        <ScrollView
+          // ref="scrollView"
+          ref={this.myRef}
+          style={styles.foregroundContainer}
+          showsVerticalScrollIndicator={false}
+          onScroll={e => this._onScroll(e)}
+          onScrollEndDrag={() => this._onScrollEndDrag()}
+          onScrollBeginDrag={() => this._onScrollBeginDrag()}
+        >
+          {this._renderForegroundContent()}
+        </ScrollView>
+        
       </View>
     )
   }
@@ -208,9 +289,105 @@ export default class HomePage extends React.Component {
     this.messageSubscription && this.messageSubscription.remove();
     this.propsSubscription && this.propsSubscription.remove();
   }
+
+  _renderForegroundContent() {
+    const contentHeight = scrollViewHeight; // 60 * 6 + 15 * 8; // 计算实际内容的高度
+    const bottomBlankHeight = scrollViewHeight - contentHeight > 0 ? scrollViewHeight - contentHeight : 0; // 计算底部垫补空白的高度
+    return (
+      <View>
+        <View style={styles.topBlank}>
+          
+        </View>
+        <View style={styles.foregroundContent}>
+          <TouchableHighlight
+            key={'togle_switch'}
+            underlayColor="#fff"
+            style={{ marginBottom: 15 }}
+          >
+          <View style={styles.switchArea}>
+            <Text style={styles.switchText}>水龙头开关</Text><
+            Switch style={styles.switchIcon} 
+              onTintColor='skyblue'
+              tintColor='lightgrey'
+              value={this.state.pro2_1}
+              onValueChange={value => this.changeSwitch(value)}
+              />
+          </View>
+          </TouchableHighlight>
+          <TouchableHighlight
+            key={'togle_led_display'}
+            underlayColor="#fff"
+            style={{ marginBottom: 15 }}
+          >
+          <View style={styles.switchArea}>
+            <Text style={styles.switchText}>指示灯开关</Text>
+            <Switch style={styles.switchIcon} 
+              onTintColor='skyblue'
+              tintColor='lightgrey'
+              value={this.state.pro6_1}
+              onValueChange={value => this.changeLedDisplay(value)}
+              />
+          </View>
+          </TouchableHighlight>
+          <TouchableHighlight
+            key={'change_auto_stop_min'}
+            underlayColor="#fff"
+            style={{ marginBottom: 10 }}
+          >
+          <View style={styles.switchArea}>
+            <Text style={styles.switchText}>自动停止</Text>
+            <NumberSpinner
+              style={{width:80, height:100}}
+              maxValue={60}
+              minValue={1}
+              interval={1}
+              defaultValue={this.state.pro6_2}
+              unit={"分钟"}
+              onNumberChanged={value => this.changeAutoStop(value.newValue) }
+            />
+          </View>
+        </TouchableHighlight>
+        </View>
+        <View style={[styles.bottomBlank, { height: bottomBlankHeight }]} >
+          
+        </View>
+      </View>
+    )
+  }
+
+  _onScrollBeginDrag() {
+    this.setState({ scrollMode: true });
+  }
+
+  /**
+   * 综合考虑速度和位移
+   */
+  _onScroll(e) {
+    this.contentOffsetY = e['nativeEvent']['contentOffset']['y'] + e['nativeEvent']['velocity']['y'] * 300;
+  }
+
+  _onScrollEndDrag() {
+    const { current } = this.myRef || {}
+    if (this.contentOffsetY < contentInsetBottom * 0.5) {
+      current && current.scrollTo({ x: 0, y: 0, animated: true });
+      this.setState({ scrollMode: false });
+    }
+    else if (this.contentOffsetY < contentInsetBottom) {
+      console.log("top");
+      current && current.scrollTo({ x: 0, y: contentInsetBottom, animated: true });
+    }
+  }
+
 }
 
+
+
 var styles = StyleSheet.create({
+  flower:{
+    bottom: 90,
+    alignItems: 'center',
+    position: "absolute",
+  },
   mainContent: {
     marginTop: 25,
     marginLeft: 25,
@@ -248,9 +425,11 @@ var styles = StyleSheet.create({
     borderTopRightRadius: 20
   },
   switchArea: {
-    marginTop: 35,
+    marginTop: 10,
+    marginBottom: 10,
     marginLeft: 25,
     marginRight: 25,
+    textAlignVertical: "center",
     flexDirection: 'row',
     flexWrap: 'nowrap',
     justifyContent: 'space-between'
@@ -263,5 +442,46 @@ var styles = StyleSheet.create({
     width: 50, 
     height: 25,
     marginRight: 25
+  },
+  backgroundContainer: {
+    width: windowWidth,
+    backgroundColor: 'lightpink',
+    height: backgroundContentHeight,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  backgroundContent: {
+    backgroundColor: '#fff',
+    height: backgroundContentHeight / 2,
+    width: windowWidth / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  foregroundContainer: {
+    position: "absolute",
+    backgroundColor: "transparent",
+    // backgroundColor: "green",
+    height: scrollViewHeight,
+  },
+  topBlank: {
+    height: backgroundContentHeight,
+    backgroundColor: 'transparent',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  foregroundContent: {
+    width: windowWidth,
+    backgroundColor: '#fff',
+    padding: 15,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderColor: '#EFEFEF',
+    borderWidth: 1,
+    borderBottomWidth: 0,
+  },
+  bottomBlank: {
+    backgroundColor: '#EEEEEE',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
